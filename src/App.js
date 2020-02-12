653abe5cbb47a1c30d6f2add36339b12efa82429
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-02-12 16:23:37 +0100

    uploading final live demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Country from './Country'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Ponemos App como un class component porque queremos acceder al estado
+class App extends Component {
+
+  state = {
+    countriesList: ['Francia', 'Chile', 'China', 'Australia', 'Guinea', 'Nigeria', 'Portugal', 'Alemania', 'Brasil', 'Andorra'],
+    inputText: ''
+  }
+
+  async changeInputText(info){
+    await this.setState({
+      inputText: info
+    })
+  }
+
+  addCountry =()=>{
+    let newArray = [...this.state.countriesList]
+    newArray.push(this.state.inputText)
+    this.setState({
+      countriesList: newArray
+    })
+  }
+
+  searchAndDelete(thisCountry){
+    let newArray = this.state.countriesList.filter((country)=>{
+      return country !== thisCountry
+    })
+
+    this.setState({
+      countriesList: newArray
+    })
+
+  }
+
+  renderCountries(){
+
+    let filteredArray = this.state.countriesList.filter((country)=>{
+      return country.toLowerCase().includes(this.state.inputText.toLowerCase())
+    })
+
+    if(filteredArray.length === 0){
+      return (
+        <div>
+          <p>No hay paises con este nombre. ¿Quieres añadirlo?</p>
+          <button onClick={()=>this.addCountry()}>Añadir</button>          
+        </div>
+      )
+    }
+
+    //Hacemos un loop a los paises dentro del estado y metemos la info en Country
+    let countries = filteredArray.map((country, index)=>{
+      return (
+        <div className="blue_color">
+          <Country key={index} pais={country}/>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Red_X.svg/1024px-Red_X.svg.png" alt="Cross" onClick={()=>this.searchAndDelete(country)}/>
+        </div>
+      )
+        
+    })
+    return countries;
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <input onChange={(event)=>this.changeInputText(event.target.value)} type="text"/>
+        {this.renderCountries()}
+      </div>
+    );    
+  }
 }
 
 export default App;
---
+++ src/Country.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Country = (props)=>{
+
+  let deleteCountry = ()=>{
+    
+  }
+
+  return(
+    <div>
+      <p id={props.pais}>{props.pais}</p>
+    </div>
+  )
+}
+
+export default Country
